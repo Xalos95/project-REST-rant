ad624c8baf2b0ac0043b2274109083d4f8f8b58d
Xalos95 <142462852+Xalos95@users.noreply.github.com>
2023-11-13 19:57:39 -0800

    adding bootstrap grid to index page

--- views/default.jsx
+++ views/default.jsx
@@ -4,7 +4,7 @@ function Def (html) {
     return (
         <html>
             <title>Title</title>
-            <link rel="stylesheet" href="" />
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" />
             <link rel="stylesheet" href="/css/style.css" />
             <body>
                 {html.children}
