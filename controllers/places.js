7bddb1b8001e62aad51678faa729f1676c0f5106
Xalos95 <142462852+Xalos95@users.noreply.github.com>
2023-11-17 22:32:57 -0800

    added the edit functionslity

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { useTransition } = require("react");
 const places = require("../models/places.js");
 
 ///Get places
@@ -10,6 +11,7 @@ router.get("/new", (req, res) => {
   res.render("places/new");
 });
 
+//post
 router.post("/", (req, res) => {
   if (!req.body.pic) {
     //Default image if one is not provided
@@ -23,8 +25,35 @@ router.post("/", (req, res) => {
   }
   places.push(req.body);
   res.redirect("/places");
-});
+});useTransition
 
+//put
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+      if (!req.body.pic) {
+          req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
+      places[id] = req.body
+      res.redirect(`/places/${id}`)
+  }
+})
+
+
+
+//show
 router.get("/:id", (req, res) => {
   let id = Number(req.params.id);
   if (isNaN(id)) {
@@ -36,6 +65,7 @@ router.get("/:id", (req, res) => {
   }
 });
 
+//edit
 router.get('/:id/edit', (req, res) => {
   let id = Number(req.params.id)
   if(isNaN(is)) {
@@ -50,6 +80,7 @@ router.get('/:id/edit', (req, res) => {
   
 })
 
+//delete
 router.delete("/:id", (req, res) => {
   let id = Number(req.params.id);
   if (isNaN(id)) {
